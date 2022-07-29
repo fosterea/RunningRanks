ccee4cac77be5a23cf51ec8877fafc733a852350
fosterea <foster.angus@gmail.com>
2022-07-29 16:55:54 -0700

    add README

--- functions/main.py
+++ functions/main.py
@@ -24,7 +24,9 @@ events = [
     '1500m/women',
     '5000m/women',
     '10000m/women',
+    '100mh/women',
     '400mh/women',
+    '3000msc/women',
     'marathon/women',
     '100m/men',
     '200m/men',
@@ -33,7 +35,9 @@ events = [
     '1500m/men',
     '5000m/men',
     '10000m/men',
+    '110mh/men',
     '400mh/men',
+    '3000msc/men',
     'marathon/men',
 ]
 
